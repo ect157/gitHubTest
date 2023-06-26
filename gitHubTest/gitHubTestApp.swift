3aa2f3c0bc686b679416c16f4b656ff595cb1800
Emma Tse <eclairnc@gmail.com>
2023-06-26 15:28:31 -0400

    Initial Commit

---
+++ gitHubTest/gitHubTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  gitHubTestApp.swift
+//  gitHubTest
+//
+//  Created by scholar on 6/26/23.
+//
+
+import SwiftUI
+
+@main
+struct gitHubTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
